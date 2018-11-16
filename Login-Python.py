486525d50ff239d449eb115c6ba9d2cf9a244a1b
COHDSE182F-010REPO <44773021+COHDSE182F-010REPO@users.noreply.github.com>
2018-11-16 16:17:54 +0530

    Add files via upload

---
+++ Login-Python.py
@@ -0,0 +1,15 @@
+﻿
+
+
+userName=input("Enter your user name:")
+password=input("Enter your password:")
+
+if userName == "admin":
+    if password=="123":
+        print("correct usarname & password, Login Successful")
+    else:
+        print("Wrong Password")
+elif password=="123":
+    print("Wrong username")
+else:
+    print("Wrong usarname & password, Login Fail")  
